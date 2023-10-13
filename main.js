aa212c1bdd09e1ba8cbf18f19034970fed8a7499
Stanleys75 <stanleysaputra25@gmail.com>
2023-10-13 11:14:24 +0700

    My Portofolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["5th Student", "Computer Science Major", "Learner Person"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
